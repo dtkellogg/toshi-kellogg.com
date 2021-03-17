978073de1ccc48d232dcad60b16dfc8a1cc7ade5
Toshi <dtkellogg10@gmail.com>
2021-03-16 22:03:41 -0700

    Home screen word toshi increased z-index so it's not overlappped by the giant TK on smaller screens

--- frontend/src/App/screens/HomeScreen.js
+++ frontend/src/App/screens/HomeScreen.js
@@ -23,7 +23,7 @@ function HomeScreen() {
         <div className="fadeInAnimated--0 home__text home__text--1 text-size-3">Hello,</div>
         <div className="home__text home__text--2 text-size-0">
           <span className="fadeInAnimated--1 home__text--im">I'm</span>
-          <span className="fadeInAnimated--1-5 home__text--name">Toshi</span>
+          <span className="fadeInAnimated--1-5 home__text--toshi">Toshi</span>
         </div>
         <div className="fadeInAnimated--2-25 home__text home__text--3 text-size-1">I like to build things for the web.</div>
         <div className="fadeInAnimated--3 home__text home__text--5 text-size-3">I am a novice web developer based in San Francisco, specializing in MERN stacks.</div>
