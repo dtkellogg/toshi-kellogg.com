5b6b8c1f662954221f2e851e75a517d704fddbad
Toshi <dtkellogg10@gmail.com>
2021-03-17 14:56:39 -0700

    Mapbox - Trying to fix by having the map reload once the page loads (because its CSS had display initally set to none

--- frontend/src/App/components/Map.js
+++ frontend/src/App/components/Map.js
@@ -1,34 +1,55 @@
 import React from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
+// import ReactMapboxGl, { Marker } from 'react-mapbox-gl'
+import * as MapboxGl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const { REACT_APP_MAPBOX_TOKEN, REACT_APP_MAPBOX_STYLES } = process.env;
 
 
 
 export default function Map() {
-  
   const [viewport, setViewport] = React.useState({
     latitude: 38.0858,
     longitude: -122.018,
     width: "100%",
     height: "100%",
     zoom: 8.3,
-    display: "block"
+    display: "block",
   });
 
+  // // your code that shows the map div
+  // $("#map-div").show();
 
-  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
-  console.log({ REACT_APP_MAPBOX_TOKEN });
+  // // detect the map's new width and height and resize it
+  // map.resize();
+
+  // var mapDiv = document.getElementById("map");
+
+  // React.useEffect(()=> {
+  //   if (mapDiv.style.visibility === true) map.resize();
+  // }, [mapDiv])
+
   
+
+  const onLoaded = (map) => {
+    map.resize();
+  };
+
+  console.log(process.env.REACT_APP_MAPBOX_TOKEN);
+  console.log({ REACT_APP_MAPBOX_TOKEN });
+  console.log(REACT_APP_MAPBOX_TOKEN, REACT_APP_MAPBOX_STYLES);
+
   return (
     <div className="map">
       <ReactMapGL
         {...viewport}
-        mapboxApiAccessToken="pk.eyJ1IjoiZHRrZWxsb2dnIiwiYSI6ImNrYTZiYXV0YzA2bWwzMG1uamF3ZWg3eG0ifQ.X5AXY5FGVc9I5vRzczRieg"
-        mapStyle="mapbox://styles/dtkellogg/ckm7b4o3j0pss17k440gn2z9f"
+        mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
+        mapStyle={REACT_APP_MAPBOX_STYLES}
         onViewportChange={(viewport) => setViewport(viewport)}
         scrollZoom={true}
         userInteractionEnabled={true}
+        onStyleLoad={(map) => onLoaded(map)}
       >
         <Marker latitude={37.9158} longitude={-122.158}>
           <div className="text-size-3 map__marker--lafayette">Lafayette</div>
