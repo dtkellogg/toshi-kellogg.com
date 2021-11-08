0bfefc12eb617d7318a700bbfcd671ac8e771a73
Toshi <dtkellogg10@gmail.com>
2021-11-08 15:36:54 -0800

    New Skills figure created

--- frontend/src/App/screens/AboutScreen.js
+++ frontend/src/App/screens/AboutScreen.js
@@ -5,6 +5,7 @@ import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 
 // components
 import SkillsFigure from '../components/SkillsFigure'
+import SkillsBarGraph from '../components/SkillsBarGraph'
 
 // images
 // import aboutImage from '../img/aboutImage.jpg'
@@ -39,7 +40,6 @@ function AboutScreen() {
   }, [])
 
   return (
-    <>
     <section className="about__container">
       <section className="about__container--left">
         <header className="about__header fadeInAnimated--0">
@@ -55,27 +55,33 @@ function AboutScreen() {
           <br />
           <span className="about__text--2 text-size-3 fadeInAnimated--2">
             Coming from a bicultural background, thinking in different languages comes intuitively. Having completed my bachelor’s at UC Davis in Chemistry and run my own college level tutoring service, I have fine tuned my ability to break down a problem into simple steps and write code efficiently and logically.
-          </span>
+          {/* </span>
           <br />
           <br />
-          <span className="about__text--3 text-size-3 fadeInAnimated--3">
+          <span className="about__text--3 text-size-3 fadeInAnimated--3"> */}
             Besides my love for mastering the material, my success as a programmer has come from an innate ability to maintain patience, stay curious, and listen attentively.
           </span>
           <br />
           <br />
-          <span className="about__text--4 text-size-3 fadeInAnimated--4">
+          <span className="about__text--4 text-size-3 fadeInAnimated--3">
             When I am not programming, I enjoy spending my time running, reading a novel, and traveling.
           </span>
         </section>
       </section>
 
-      <section className="about__container--right fadeInAnimated--4">
-        <figure className="about__container--img">
+      <section className="about__container--right">
+        <header className="skills-graph__header fadeInAnimated--4">
+          <h1 className="text-size-1">
+            Skills
+          </h1>
+        </header>
+        <SkillsBarGraph />
+        {/* <figure className="about__container--img">
           <img
             src={image.src}
             alt="My mom, dog and I (circa 2010)"
             className={image.src === toshiHeadshot ? "about__image--toshi-headshot" : "about__image--about"}
-          />
+          /> */}
           {/* <span className="about__image--background" /> */}
           {/* <div className="about__arrows--container">
             <button
@@ -102,47 +108,9 @@ function AboutScreen() {
               />
             </button>
           </div> */}
-        </figure>
+        {/* </figure> */}
       </section>
     </section>
-    <section className="skills__container">
-      <section className="skills__container--left">
-        <header className="skills__header fadeInAnimated--5">
-          <h1 className="text-size-1">Skills & Experience</h1>
-        </header>
-        <p className="skills__paragraph">
-          <span className="skills__paragraph--1 text-size-3 fadeInAnimated--6">
-            My expertise is building web applications using a MERN stack.
-            I have extensive knowledge and experience in <abbr title="HyperText Markup Language">HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>
-            (<abbr title="Syntactically Awesome Style Sheets" className="skills__blue-text">SASS</abbr>), JavaScript 
-            (<span className="skills__blue-text">React</span> and <span className="skills__blue-text">Node</span>), and Redux.
-          </span>
-          <br />
-          <br />
-          <span className="skills__paragraph--2 text-size-3 fadeInAnimated--7">
-            I am also able to solve algorithm problems using all of the standard strategies. 
-            These include recursion, <abbr title="Depth-First Search">DFS</abbr>, <abbr title="Breadth-First Search">BFS</abbr>, <abbr title="Dynamic Programming">DP</abbr>, binary search, hash tables, arrays, and string manipulation.
-          </span>
-          <br />
-          <br />
-          <span className="skills__paragraph--3 text-size-3 fadeInAnimated--8">
-            I enjoy writing code cleanly, and through my ability to patiently observe and learn from other programmers’ 
-            work, I am able to write code following a uniform pattern, which is both necessary and highly effective for 
-            large-scale stacks.
-          </span>
-          <br />
-          <br />
-          <span className="skills__paragraph--4 text-size-3 fadeInAnimated--9">
-            Please check out my projects to learn more about what I have created in JavaScript.
-          </span>
-        </p>
-      </section>
-
-      <section className="skills__container--right fadeInAnimated--5">
-        <SkillsFigure />
-      </section>
-    </section>
-    </>
   );
 }
 
