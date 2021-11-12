dc9539657e0a7fe257c2313ecdd791f7141d4769
Toshi <dtkellogg10@gmail.com>
2021-11-12 10:45:26 -0800

    Added a cleanup function to useEffect so that the modal closes when changing screens.

--- frontend/src/App/screens/ProjectsScreen.js
+++ frontend/src/App/screens/ProjectsScreen.js
@@ -1,21 +1,29 @@
 // react
 import React, {useState, useEffect} from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 // components
 import Modal from '../components/Modal'
 import Hero from '../components/Hero'
 import Projects from '../components/Projects'
 
+// actions
+import { modalToggleOpen } from "../actions/modalActions";
+
 
 export default function ProjectsScreen() {
   const [project, setProject] = useState(""); // eslint-disable-line no-unused-vars
+  const dispatch = useDispatch()
 
   const modalIsOpen = useSelector((state) => state.modalIsOpen);
   const { isOpen } = modalIsOpen;
 
   useEffect(() => {
     document.title = "Toshi Kellogg | Developer";
+
+    return () => {
+      dispatch(modalToggleOpen());
+    }
   }, [])
 
   return (
