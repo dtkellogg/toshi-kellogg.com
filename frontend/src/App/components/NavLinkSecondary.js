41fbb60cf4900c59b29336731b6e8d2cca7a5e80
Toshi <dtkellogg10@gmail.com>
2022-01-28 17:26:56 -0800

    Improved Accessibility Lighthouse rating by putting Nav Links in li's.

--- frontend/src/App/components/NavLinkSecondary.js
+++ frontend/src/App/components/NavLinkSecondary.js
@@ -6,7 +6,6 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 // hooks
 import useHover from "../hooks/useHover";
 
-
 function NavLinkSecondary({ text }) {
   const [hovering, attrs] = useHover();
 
@@ -18,17 +17,19 @@ function NavLinkSecondary({ text }) {
   const Icon = icons[text]
 
   return (
-    <a 
-      className="nav__secondary--item" 
-      href={text === "github" ? "https://github.com/dtkellogg" : "https://linkedin.com/in/damian-toshiya-kellogg/"} 
-      {...attrs}
-    >
-      <Icon
-        size={30}
-        fill={!hovering ? "var(--grey-6)" : "var(--grey-4"}
-        className="nav__secondary--icon"
-      />
-    </a>
+    <li  className="nav__secondary--item">
+      <a 
+        
+        href={text === "github" ? "https://github.com/dtkellogg" : "https://linkedin.com/in/damian-toshiya-kellogg/"} 
+        {...attrs}
+      >
+        <Icon
+          size={30}
+          fill={!hovering ? "var(--grey-6)" : "var(--grey-4"}
+          className="nav__secondary--icon"
+        />
+      </a>
+    </li>
   );
 }
 
