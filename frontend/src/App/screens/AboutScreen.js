1ee0888749541c0608ea918a8d64427e228df9bf
Toshi <dtkellogg10@gmail.com>
2023-02-02 22:27:00 -0400

    (WIP) About - rewritting blurb about myself

--- frontend/src/App/screens/AboutScreen.js
+++ frontend/src/App/screens/AboutScreen.js
@@ -22,13 +22,13 @@ export default function AboutScreen() {
         </header>
         <section className="about__text">
           <p className="about__text--1">
-            Hello, I’m Toshi. I’m a self taught full stack programmer with a passion for data structures and algorithms.
+            Hello, I'm Toshi. I'm a self taught full stack programmer with a passion for learning new technologies.
           </p>
           <p className="about__text--2">
-            Coming from a bicultural background, thinking in different languages comes intuitively. Having completed my bachelor’s at UC Davis in chemistry and running my own college level tutoring business, I have fine tuned my ability to <em>break down problems</em> - translating to <strong>effective and logical code.</strong> My success as a programmer also comes from an innate ability to listen attentively, and always maintain my patience and curiosity.
+            Three years ago, I decided to pursue a career in programming. I have been able to teach myself almost everything I know, and have worked professionally as a full stack developer for a little under a year. My success as a programmer comes from my background in STEM and as a teacher, as well as my patience and curiosity.
           </p>
           <p className="about__text--3">
-            When I am not programming, I love to spend my time reading, running, rock climbing, scuba diving and traveling.
+            When I am not programming, I love to spend my time traveling.
           </p>
         </section>
       </section>
